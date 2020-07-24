ff12c79e9e4299acc428ea45fcbdd8152875d5ab
Mark Wild <mark.wild189@gmail.com>
2020-07-24 15:08:56 -0700

    Updates

---
+++ precache-manifest.681f0bfe27d0fd0efbfcedd84f7b420a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd785d95225088a8121a3a81d1d070d",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "4991acdbe60907dffdf1",
+    "url": "/covid-tracker/static/css/main.95e5aa3d.chunk.css"
+  },
+  {
+    "revision": "d969f86e63122300223b",
+    "url": "/covid-tracker/static/js/2.7326f2b8.chunk.js"
+  },
+  {
+    "revision": "e2c2a6235d96dd5865367cee8b1a2e0f",
+    "url": "/covid-tracker/static/js/2.7326f2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4991acdbe60907dffdf1",
+    "url": "/covid-tracker/static/js/main.180c451b.chunk.js"
+  },
+  {
+    "revision": "c53a344265904753956a",
+    "url": "/covid-tracker/static/js/runtime-main.ee22e45f.js"
+  },
+  {
+    "revision": "1778b5ab55fe14563fe8b3946940da95",
+    "url": "/covid-tracker/static/media/001-protection.1778b5ab.png"
+  },
+  {
+    "revision": "81c952c1eb0423accb5abc49363c876c",
+    "url": "/covid-tracker/static/media/011-No touch.81c952c1.png"
+  },
+  {
+    "revision": "a036eee45eddfe5ede7170aa8677e459",
+    "url": "/covid-tracker/static/media/012-calendar.a036eee4.png"
+  },
+  {
+    "revision": "b26fff7a9b111ad98c9c249f9544d959",
+    "url": "/covid-tracker/static/media/021-washing hands.b26fff7a.png"
+  },
+  {
+    "revision": "c588e3d898101407239edcfe1e84d7fe",
+    "url": "/covid-tracker/static/media/026-no group.c588e3d8.png"
+  },
+  {
+    "revision": "0d30aad98979d7276958f9637a416122",
+    "url": "/covid-tracker/static/media/041-news.0d30aad9.png"
+  },
+  {
+    "revision": "2c35bd94daf38f8f26243c29e0809d49",
+    "url": "/covid-tracker/static/media/usFlag.2c35bd94.svg"
+  }
+]);
